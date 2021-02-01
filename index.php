331c290bb6fe388bb4dd969541e2e60cc3d9d3cf
Clems-web <clement.servais1@gmail.com>
2021-02-01 09:42:42 +0100

    TERMINADO

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const NUMBER = 12;
+const NUMBER2 = 2;
+echo NUMBER*NUMBER2 . "<br>";
+const texte = "c'est cool PHP";
+
+echo ((NUMBER+NUMBER2)* strlen(texte)) . "<br>";
 
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+
+echo basename(__FILE__). "<br>";
+echo basename(__DIR__). "<br>";
+echo basename(__LINE__). "<br>";
+echo basename(__FUNCTION__). "<br>";
+echo basename(__CLASS__). "<br>";
+echo basename(__METHOD__). "<br>";
+echo basename(__NAMESPACE__). "<br>";
+echo basename(__TRAIT__). "<br>";
